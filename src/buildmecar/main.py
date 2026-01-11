0c2995c2970fcea10f82957cd183a6a385097f01
Ludwik Bielczynski <lubielczynski@gmail.com>
2026-01-11 17:27:00 +0100

    feat: Move styling to css file

--- src/buildmecar/main.py
+++ src/buildmecar/main.py
@@ -25,7 +25,7 @@ print(f"HAS_CAMERA_ON: {HAS_CAMERA_ON}")
 DEFAULT_MOTOR_SPEED = 98
 DEFAULT_MOTOR_PULSE = 1000
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 # Global state for camera streaming
 camera_streaming_enabled = False
