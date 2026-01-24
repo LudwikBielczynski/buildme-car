33b615c9b201d46b972634d244bb2999fec774af
Ludwik Bielczynski <lubielczynski@gmail.com>
2026-01-24 13:35:03 +0100

    feat: Correct turning

--- src/buildmecar/car.py
+++ src/buildmecar/car.py
@@ -33,6 +33,13 @@ class Car:
             self.motor_left_rear = PassiveMotor("C")
             self.motor_left_front = PassiveMotor("B")
 
+            self.directions_correction = MotorDirections(
+                right_rear=1,
+                right_front=1,
+                left_rear=-1,
+                left_front=-1,
+            )
+
     def set_speed(self, port: int, speed: int = DEFAULT_MOTOR_SPEED):
         port.start(int(speed))
 
@@ -42,18 +49,30 @@ class Car:
                 f"SIMULATE: Motor command - directions={directions}, speed={speed}, time={time_ms}ms"
             )
             return
-        self.set_speed(self.motor_right_rear, directions.right_rear * speed)
-        self.set_speed(self.motor_right_front, directions.right_front * speed)
-        self.set_speed(self.motor_left_rear, directions.left_rear * speed)
-        self.set_speed(self.motor_left_front, directions.left_front * speed)
+        self.set_speed(
+            self.motor_right_rear,
+            self.directions_correction.right_rear * directions.right_rear * speed,
+        )
+        self.set_speed(
+            self.motor_right_front,
+            self.directions_correction.right_front * directions.right_front * speed,
+        )
+        self.set_speed(
+            self.motor_left_rear,
+            self.directions_correction.left_rear * directions.left_rear * speed,
+        )
+        self.set_speed(
+            self.motor_left_front,
+            self.directions_correction.left_front * directions.left_front * speed,
+        )
         time.sleep(time_ms / DEFAULT_MOTOR_PULSE)
 
     def front(self, speed: int = DEFAULT_MOTOR_SPEED, time_ms: int = 0) -> None:
-        directions = MotorDirections(1, 1, -1, -1)
+        directions = MotorDirections(1, 1, 1, 1)
         self._run_motor(directions, speed, time_ms)
 
     def rear(self, speed: int = DEFAULT_MOTOR_SPEED, time_ms: int = 0) -> None:
-        directions = MotorDirections(-1, -1, 1, 1)
+        directions = MotorDirections(-1, -1, -1, -1)
         self._run_motor(directions, speed, time_ms)
 
     def right(self, speed=DEFAULT_MOTOR_SPEED, time_ms=0):
@@ -65,11 +84,11 @@ class Car:
         self._run_motor(directions, speed, time_ms)
 
     def front_left(self, speed: int = DEFAULT_MOTOR_SPEED, time_ms: int = 0) -> None:
-        directions = MotorDirections(1, -1, 1, -1)
+        directions = MotorDirections(1, 1, -1, -1)
         self._run_motor(directions, speed, time_ms)
 
     def front_right(self, speed: int = DEFAULT_MOTOR_SPEED, time_ms: int = 0) -> None:
-        directions = MotorDirections(1, 1, -1, -1)
+        directions = MotorDirections(-1, -1, 1, 1)
         self._run_motor(directions, speed, time_ms)
 
     def rear_left(self, speed: int = DEFAULT_MOTOR_SPEED, time_ms: int = 0) -> None:
