447aff7a684b99519df9b54f5a6fa600b67fc69c
Ludwik Bielczynski <lubielczynski@gmail.com>
2026-01-24 14:37:46 +0100

    feat: Log on werkzeug logger

--- src/buildmecar/car.py
+++ src/buildmecar/car.py
@@ -1,7 +1,10 @@
+import logging
 import time
 from typing import NamedTuple
 
 from buildhat import Hat, PassiveMotor
+
+logger = logging.getLogger("werkzeug")
 
 
 class MotorDirections(NamedTuple):
@@ -42,7 +45,7 @@ class Car:
 
     def set_speed(self, motor: PassiveMotor, speed: int = DEFAULT_MOTOR_SPEED):
         motor.start(int(speed))
-        print(f"Motor on port {motor.port} set to speed {speed}")
+        logger.info(f"Motor on port {motor.port} set to speed {speed}")
 
     def _run_motor(self, directions: MotorDirections, speed: int, time_ms: int) -> None:
         """
